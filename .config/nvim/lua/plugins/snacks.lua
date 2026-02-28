1fb1bf3853933d9f7f5b0bfb1c4d6867171145c1
gstrainovic <g.strainovic@gmail.com>
2026-02-28 01:14:54 +0100

    nvim: explorer preview standardmäßig an (layout.preview = true)

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -12,22 +12,7 @@ return {
       picker = {
         sources = {
           explorer = {
-            preview = true, -- Bild-Preview standardmäßig an
-            actions = {
-              -- Alt+P als Toggle (zusätzlich zu P)
-              toggle_preview_alt = {
-                action = function(picker)
-                  picker:action("toggle_preview")
-                end,
-              },
-            },
-            win = {
-              list = {
-                keys = {
-                  ["<A-p>"] = "toggle_preview",
-                },
-              },
-            },
+            layout = { preset = "sidebar", preview = true },
           },
         },
       },
