b2fcad7e62d16530a099a6d39bdaf9f9831f07cd
gstrainovic <g.strainovic@gmail.com>
2026-02-28 01:09:19 +0100

    nvim: entferne ip-Keymap (hover funktioniert nicht)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Bild-Popup: Zeigt Bild unter Cursor in einem Floating-Fenster (Ghostty/KGP)
-vim.keymap.set("n", "<leader>ip", function()
-  Snacks.image.hover()
-end, { desc = "Image Preview Popup" })
